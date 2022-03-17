coreos/actions-lib

commit da6a14c893378625d0b35c50d4446d390a2f81de

Fix change detection at EOF; add tests

check-diff.py
@@ -26,9 +26,6 @@ def diff(canon_path, left_lines, right_lines, severity, output=sys.stdout):
     seq = difflib.SequenceMatcher(a=left_lines, b=right_lines, autojunk=False)
     ok = True
     for first, second in itertools.pairwise(seq.get_matching_blocks()):
-        if second.size == 0:
-            # sentinel value
-            break
         ok = False
         left_end = first.a + first.size
         right_end = first.b + first.size
@@ -38,7 +35,7 @@ def diff(canon_path, left_lines, right_lines, severity, output=sys.stdout):
             annotate(output, canon_path, right_end, right_start, severity, 'Unexpected change')
         elif right_end != right_start:
             annotate(output, canon_path, right_end, right_start, severity, 'Unexpected addition')
-        else:
+        elif left_end != left_start:
             # message before the removal is a bit more obvious than after it
             annotate(output, canon_path, right_start - 1, right_start, severity, 'Unexpected removal on next line')
     return ok
@@ -87,6 +84,22 @@ def selftest():
 ::alert! file=a/b/c,line=7,endLine=7,title=Line 7::Unexpected addition
 ::alert! file=a/b/c,line=8,endLine=8,title=Line 8::Unexpected removal on next line
 ''')
+    # Check EOF behavior
+    selftest_one(
+        ['one', 'two', 'three', 'four'],
+        ['one', 'two', 'five', 'six'],
+        '::alert! file=a/b/c,line=3,endLine=4,title=Lines 3-4::Unexpected change\n'
+    )
+    selftest_one(
+        ['one', 'two'],
+        ['one', 'two', 'three', 'four'],
+        '::alert! file=a/b/c,line=3,endLine=4,title=Lines 3-4::Unexpected addition\n'
+    )
+    selftest_one(
+        ['one', 'two', 'three', 'four'],
+        ['one', 'two'],
+        '::alert! file=a/b/c,line=2,endLine=2,title=Line 2::Unexpected removal on next line\n'
+    )
 
 
 def main():

commit e198b89795e8d0c8014b041670101bcfef5af9da

Fix spurious non-zero exit status
Fixes: da6a14c893 ("Fix change detection at EOF; add tests")

check-diff.py
@@ -37,18 +37,20 @@ def diff(canon_path, left_lines, right_lines, severity, output=sys.stdout):
     # end, to simplify handling of disjoint files where one of them is empty
     matching.insert(0, difflib.Match(0, 0, 0))
     for first, second in itertools.pairwise(matching):
-        ok = False
         left_end = first.a + first.size
         right_end = first.b + first.size
         left_start = second.a
         right_start = second.b
         if right_end != right_start and left_end != left_start:
             annotate_line(output, canon_path, right_end, right_start, severity, 'Unexpected change')
+            ok = False
         elif right_end != right_start:
             annotate_line(output, canon_path, right_end, right_start, severity, 'Unexpected addition')
+            ok = False
         elif left_end != left_start:
             # message before the removal is a bit more obvious than after it
             annotate_line(output, canon_path, right_start - 1, right_start, severity, 'Unexpected removal on next line')
+            ok = False
     return ok
 
 